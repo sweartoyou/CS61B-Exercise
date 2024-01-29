1af79e191cb4050a43de9900cee9e470cd27dccd
Menghan Tian <tianmenghan@baijia.com>
2024-01-29 11:04:31 +0800

    proj1b commit

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,29 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> res = new ArrayDeque<>();
+        for (int i = 0; i < word.length(); i ++ ) {
+            res.addLast(word.charAt(i));
+        }
+        return res;
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque<Character> d = wordToDeque(word);
+        while (d.size() > 1) {
+            if (d.removeFirst() != d.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> d = wordToDeque(word);
+        while (d.size() > 1) {
+            if (!cc.equalChars(d.removeFirst(), d.removeLast())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
