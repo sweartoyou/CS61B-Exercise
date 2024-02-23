cd0bb7fcf33cc03376133a864e16990be8317049
sweartoyou <1416186252@qq.com>
2024-02-23 17:11:32 +0800

    lab11 start

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
 /**
  *  @author Josh Hug
  */
@@ -8,6 +12,23 @@ public class MazeAStarPath extends MazeExplorer {
     private int t;
     private boolean targetFound = false;
     private Maze maze;
+
+    private class Node implements Comparable {
+        private int v;
+        private int priority;
+
+        public Node(int v) {
+            this.v = v;
+            this.priority = distTo[v] + h(v);
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Node n = (Node) o;
+            return this.priority - n.priority;
+        }
+    }
+
 
     public MazeAStarPath(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -20,7 +41,11 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        int sourcex = maze.toX(v);
+        int soucexy = maze.toY(v);
+        int targetx = maze.toX(t);
+        int targety = maze.toY(t);
+        return Math.abs(sourcex - targetx) + Math.abs(soucexy - targety);
     }
 
     /** Finds vertex estimated to be closest to target. */
@@ -31,7 +56,26 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Performs an A star search from vertex s. */
     private void astar(int s) {
-        // TODO
+        MinPQ<Node> pq = new MinPQ<>();
+        Node start = new Node(s);
+        pq.insert(start);
+        marked[s] = true;
+        while (!pq.isEmpty()) {
+            Node cur = pq.delMin();
+            for (int w : maze.adj(cur.v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    distTo[w] = distTo[cur.v] + 1;
+                    edgeTo[w] = cur.v;
+                    announce();
+                    if (w == t) {
+                        return ;
+                    } else {
+                        pq.insert(new Node(w));
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import java.util.Random;
+
 /**
  *  @author Josh Hug
  */
@@ -9,6 +11,8 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] pathTo;
+    private boolean circleFound = false;
 
     public MazeCycles(Maze m) {
         super(m);
@@ -16,9 +20,41 @@ public class MazeCycles extends MazeExplorer {
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        pathTo = new int[maze.V()];
+        Random rand = new Random();
+        int sourceX = rand.nextInt(maze.N());
+        int sourceY = rand.nextInt(maze.N());
+        int s = maze.xyTo1D(sourceX, sourceY);
+        pathTo[s] = s;
+        dfs(s);
+        announce();
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+
+        for (int w : maze.adj(v)) {
+            if (circleFound) {
+                return;
+            }
+
+            if (!marked[w]) {
+                pathTo[w] = v;
+                dfs(w);
+            } else if (w != pathTo[v]) {
+                pathTo[w] = v;
+
+                int cur = v;
+                edgeTo[cur] = pathTo[cur];
+                while (cur != w) {
+                    cur = pathTo[cur];
+                    edgeTo[cur] = pathTo[cur];
+                }
+                circleFound = true;
+                return;
+            }
+        }
+    }
 }
 
