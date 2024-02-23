80fca3f0b1fde9a848379110b23d99ad73841a31
sweartoyou <1416186252@qq.com>
2024-02-23 14:57:31 +0800

    fix a bug

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -201,10 +201,11 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
     public void changePriority(T item, double priority) {
         for (int i = 1; i <= size; i ++ ) {
             if (contents[i].myItem.equals(item)) {
-                contents[i].myPriority = priority;
                 if (priority > contents[i].myPriority) {
+                    contents[i].myPriority = priority;
                     sink(i);
                 } else {
+                    contents[i].myPriority = priority;
                     swim(i);
                 }
             }
